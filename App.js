660613b7a9a3dce113a8c25591e78bf2c905364e
Jessica Paul <paul.jessica95@gmail.com>
2021-05-05 18:23:39 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Dice from './App/components/Dice';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yahtzee!</Text>
+      <Dice />
+      <Dice />
+      <Dice />
+      <Dice />
+      <Dice />
       <StatusBar style="auto" />
     </View>
   );
---
+++ App/components/Dice.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Dice = () => {
+    return (
+        <View>
+            <Text>Dice</Text>
+        </View>
+    );
+}
+ 
+export default Dice;
